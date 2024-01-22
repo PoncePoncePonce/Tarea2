026ec5940185469f239d7d7d5dfe4272a74eabbf
martin ponce espinoza <martin.ponce@techsoft.com.mx>
2024-01-22 13:24:39 -0700

    Agregar archivos de proyecto.

---
+++ Tarea2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tarea2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Tarea2/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Tarea2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        //Metodo que ejecuta el boton
+        private void Aceptar_Clicked(object sender, EventArgs e)
+        {
+            //Se agrega el texto del entry al label de resultado
+            label_resultado.Text = "Resultado\r\n" + entry_info.Text;
+        }
+    }
+
+}
